cd10387373f1b6481876dffff67856688d378b70
thelonius182 <cz.teamservice@gmail.com>
2016-08-31 16:49:18 +0200

    test_println complete.

--- src/main/java/nl/concertzender/salsa/epgdirect/WPManager.java
+++ src/main/java/nl/concertzender/salsa/epgdirect/WPManager.java
@@ -4,4 +4,7 @@ package nl.concertzender.salsa.epgdirect;
  * Version 1.0, 31-8-2016 by LvdA .
  */
 public class WPManager {
+    public static void main(String[] args) {
+        System.out.println("Hello World, presumably");
+    }
 }
